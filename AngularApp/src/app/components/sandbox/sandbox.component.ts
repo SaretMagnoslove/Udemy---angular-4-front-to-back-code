51e2fd82061b4a74fb18d15a155ee6f27f3d9714
SaretMagnoslove <magnoslove@gmail.com>
2018-04-14 23:11:28 +0300

    ngFor directive

--- AngularApp/src/app/components/sandbox/sandbox.component.ts
+++ AngularApp/src/app/components/sandbox/sandbox.component.ts
@@ -1,20 +1,54 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector:'sandbox',
-    templateUrl:`./sandbox.component.html`,
-    styleUrls:['./sandbox.component.css']
-    /*
-    styles: [`
-        .special{
-            color:green;
-            font-size:20px;
-            text-transform:uppercase
-        }
-    `]
-    */
+    // tslint:disable-next-line:component-selector
+    selector: 'sandbox',
+    template: `
+        <h1>Hello World</h1>
+        {{ people }}
+        <ul>
+            <li *ngFor="let person of people">
+                {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people; let i = index">
+                {{i+1}} {{person}}
+            </li>
+        </ul>
+        <ul>
+            <li *ngFor="let person of people2">
+                {{person.firstName}} {{person.lastName}}
+            </li>
+        </ul>
+        `,
 })
 
-export class SandboxComponent{
+export class SandboxComponent {
+    people = ['Londo Mollari', 'Lyta Alexander', 'Marcus Cole', 'Vir Cotto'];
+
+    people2 = [
+        {
+            firstName: 'Rick',
+            lastName: 'Grimes'
+        },
+        {
+            firstName: 'Daryl',
+            lastName: 'Dixon'
+        },
+        {
+            firstName: 'Carl',
+            lastName: 'Grimes'
+        },
+        {
+            firstName: 'Glenn',
+            lastName: 'Rhee'
+        },
+    ];
+
+    constructor() {
+        this.people [2] = 'Lyta Alexander';
+        this.people [1] = 'Marcus Cole';
+    }
 
 }
