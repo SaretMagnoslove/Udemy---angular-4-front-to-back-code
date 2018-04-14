20bf4f64f0ea8780387baac994bf58ff6d234f9d
SaretMagnoslove <magnoslove@gmail.com>
2018-04-14 19:06:11 +0300

    template files and stylesheets

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { SanboxComponent } from './components/sandbox.component';
-
+import { SandboxComponent } from './components/sandbox/sandbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-    SanboxComponent
+    AppComponent,
+    SandboxComponent
   ],
   imports: [
     BrowserModule
