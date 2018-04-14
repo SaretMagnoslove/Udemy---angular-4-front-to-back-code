e6cabdee09a9fefb1eced4b46ba2a0569f31494c
SaretMagnoslove <magnoslove@gmail.com>
2018-04-14 17:48:31 +0300

    objects and interfaces

--- AngularApp/src/app/components/sandbox.component.ts
+++ AngularApp/src/app/components/sandbox.component.ts
@@ -1,28 +1,46 @@
+import { Customer } from './customer';
 import { Component } from '@angular/core';
+
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'sandbox',
-    template: `<h1>Hello {{ name }}</h1>`
+    template: `<h1>Hello World</h1>`
 })
 
 export class SanboxComponent {
-    // tslint:disable-next-line:no-inferrable-types
-    name: string = 'Susan Ivanova';
-    // tslint:disable-next-line:no-inferrable-types
-    age: number = 35;
-    hasChildren: Boolean = true;
-    city: any = 'B5';
-    myNumbersArray: number[] = [1, 2, 3];
-    myStringsArray: string[] = ['hello', 'world'];
-    myAnyArray: any[] = ['hello', 'world', 2];
-    myTuple: [string, number] = ['hello', 3];
-    unusable: void = undefined;
-    u: undefined = undefined;
-    n: null = null;
+    customer: Customer;
+    cutomers: Customer[];
 
     constructor() {
-     this.hasChildren = false;
-    }
+        this.customer = {
+            id: 1,
+            name: 'John Sheridan',
+            email: 'john@b5.org'
+        };
 
+        this.cutomers = [
+            {
+                id: 1,
+                name: 'John Sheridan',
+                email: 'john@b5.org'
+            },
+            {
+                id: 2,
+                name: 'Susan Ivanova',
+                email: 'susan@b5.org'
+            },
+            {
+                id: 3,
+                name: 'Ambassador Mollari',
+                email: 'mollari@b5.org'
+            },
+
+        ];
+
+    }
 }
+
+
+
+
