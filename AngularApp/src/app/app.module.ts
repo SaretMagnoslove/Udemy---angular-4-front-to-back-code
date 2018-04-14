344caf17c3bf7617f725aaed3e1e3b332ab6efb2
SaretMagnoslove <magnoslove@gmail.com>
2018-04-14 16:05:40 +0300

    ADD SandBoxComponent

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SanboxComponent } from './components/sandbox.component';
 
 
 @NgModule({
   declarations: [
     AppComponent
+    SanboxComponent
   ],
   imports: [
     BrowserModule
---
+++ AngularApp/src/app/components/sandbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'sandbox',
+    template: `<h1>Hello Wold</h1>`
+})
+
+export class SanboxComponent {
+
+}
