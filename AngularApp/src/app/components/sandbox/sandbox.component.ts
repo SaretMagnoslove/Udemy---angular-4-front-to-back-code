d3c631c863d17f902a53d70cf53d9c1727463960
SaretMagnoslove <magnoslove@gmail.com>
2018-04-21 13:59:47 +0300

    keyboard input events

--- AngularApp/src/app/components/sandbox/sandbox.component.ts
+++ AngularApp/src/app/components/sandbox/sandbox.component.ts
@@ -5,22 +5,46 @@ import { Component } from '@angular/core';
     selector: 'sandbox',
     template: `
         <h1>hello world</h1>
-        <button (click)="changeValue()">Change Value</button>
-        <div *ngIf="value">
-            <h1>{{ text }}</h1>
+        <div>
+            <input type="text" (keyup)="fireEvent($event)" placeholder="keyup event">
         </div>
+        <div>
+        <input type="text" (keydown)="fireEvent($event)" placeholder="key down event">
+        </div>
+        <div>
+        <input type="text" (focus)="fireEvent($event)" placeholder="focus event">
+        </div>
+        <div>
+        <input type="text" (blur)="fireEvent($event)" placeholder="blur event">
+        </div>
+        <div>
+        <input type="text" (cut)="fireEvent($event)" placeholder="cut event">
+        </div>
+        <div>
+        <input type="text" (copy)="fireEvent($event)" placeholder="copy event">
+        </div>
+        <div>
+        <input type="text" (paste)="fireEvent($event)" placeholder="paste event">
+        </div>
+        <hr>
+
+        <div>
+            <input type="text" (keyup)="changeText($event)" placeholder="change text" value={{text}}>
+        </div>
+        <h2>{{ text }}</h2>
     `
 })
 
 export class SandboxComponent {
     // tslint:disable-next-line:no-inferrable-types
-    text: string = 'hello world';
-    // tslint:disable-next-line:no-inferrable-types
-    value: boolean = true;
+    text: string = 'hello';
 
-    changeValue() {
-        // this.value = false;
-        this.value = !this.value;
+    fireEvent(e) {
+        console.log(e.type);
+    }
+
+    changeText(e) {
+        this.text = e.target.value;
     }
 }
 
