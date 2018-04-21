b2e28c6efeb0ca9e47dba954c6973f26ad4cbbdd
SaretMagnoslove <magnoslove@gmail.com>
2018-04-21 15:11:56 +0300

    Form Submission

--- AngularApp/src/app/components/sandbox/sandbox.component.ts
+++ AngularApp/src/app/components/sandbox/sandbox.component.ts
@@ -5,21 +5,17 @@ import { Component } from '@angular/core';
     selector: 'sandbox',
     template: `
         <h1>hello world</h1>
-        <div class="container">
-            <form>
-                <div class="form-group">
-                    <label>Name</label>
-                    <input type="text" [(ngModel)]="name" name="name" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>age</label>
-                    <input type="number" [(ngModel)]="age" name="age" class="form-control">
-                </div>
-                <input type="submit" value="submit" class="btn btn-success">
-            </form>
-            <h4>{{ name }}</h4>
-            <h4>{{ age }}</h4>
-        </div>
+        <form (submit)="onSubmit()">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" class="form-control" [(ngModel)]="name" name="name">
+            </div>
+        <input type="submit" class="btn btn-success" value="submit">
+        </form>
+        <hr>
+        <ul class="list-group">
+            <li class="list-group-item" *ngFor="let user of users">{{ user }}</li>
+        </ul>
 
     `
 })
@@ -27,7 +23,11 @@ import { Component } from '@angular/core';
 export class SandboxComponent {
     // tslint:disable-next-line:no-inferrable-types
     name: string = '';
-    // tslint:disable-next-line:no-inferrable-types
-    age: number = 0;
+    users: string[] = ['Sheldon Cooper', 'John Nash', 'Albert Einstein'];
+
+    onSubmit() {
+        this.users.push(this.name);
+        this.name = '';
+    }
 }
 
