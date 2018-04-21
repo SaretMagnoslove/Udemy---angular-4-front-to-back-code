e3e495298879c4befabb52ad92691efc8a9bc371
SaretMagnoslove <magnoslove@gmail.com>
2018-04-21 17:03:04 +0300

    template driven form validation

--- AngularApp/src/app/components/sandbox/sandbox.component.ts
+++ AngularApp/src/app/components/sandbox/sandbox.component.ts
@@ -5,29 +5,63 @@ import { Component } from '@angular/core';
     selector: 'sandbox',
     template: `
         <h1>hello world</h1>
-        <form (submit)="onSubmit()">
+        <form novalidate #f="ngForm" (ngSubmit)="onSubmit(f)">
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" class="form-control" [(ngModel)]="name" name="name">
+                <input
+                    type="text"
+                    class="form-control"
+                    [(ngModel)]="user.name"
+                    name="name"
+                    #userName="ngModel"
+                    minlength="2"
+                    required
+                >
+                <div *ngIf="userName.errors?.required && userName.touched" class="alert alert-danger">name is required</div>
+                <div *ngIf="userName.errors?.minlength && userName.touched" class="alert alert-danger">at least 2 charecters</div>
             </div>
-        <input type="submit" class="btn btn-success" value="submit">
-        </form>
-        <hr>
-        <ul class="list-group">
-            <li class="list-group-item" *ngFor="let user of users">{{ user }}</li>
-        </ul>
+            <div class="form-group">
+                <label>shemail</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    [(ngModel)]="user.shemail"
+                    name="shemail"
+                    #userShemail="ngModel"
+                     required
+                >
+                <div *ngIf="userShemail.errors?.required && userShemail.touched" class="alert alert-danger">shemail is required</div>
+            </div>
+            <div class="form-group">
+                <label>phone</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    [(ngModel)]="user.phone"
+                    name="phone"
+                    #userPhone="ngModel"
+                    minlength="10"
+                >
+                <div *ngIf="userPhone.errors?.minlength && userPhone.touched" class="alert alert-danger">at least 10 charecters</div>
+            </div>
+            <input type="submit" class="btn btn-success" value="submit">
 
     `
 })
 
 export class SandboxComponent {
-    // tslint:disable-next-line:no-inferrable-types
-    name: string = '';
-    users: string[] = ['Sheldon Cooper', 'John Nash', 'Albert Einstein'];
+    user = {
+        name: '',
+        shemail: '',
+        phone: '',
+    };
 
-    onSubmit() {
-        this.users.push(this.name);
-        this.name = '';
+    onSubmit({value, valid}) {
+        if(valid) {
+            console.log(value);
+        } else {
+            console.log('form is invalid');
+        }
     }
 }
 
