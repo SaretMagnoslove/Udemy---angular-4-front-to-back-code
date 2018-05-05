2283c8b65a39b8e93a93ef7a290b8732b1062112
SaretMagnoslove <magnoslove@gmail.com>
2018-05-05 21:28:44 +0300

    Client Balance Update

--- clientpanel/src/app/services/client.service.ts
+++ clientpanel/src/app/services/client.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+// tslint:disable-next-line:import-blacklist
 import { Observable } from 'rxjs';
 import { Client } from '../models/Client';
 
@@ -9,22 +10,26 @@ export class ClientService {
   client: FirebaseObjectObservable<any>;
 
   constructor(
-    public af:AngularFireDatabase
-  ) { 
+    public af: AngularFireDatabase
+  ) {
     this.clients = this.af.list('/clients') as FirebaseListObservable<Client[]>;
   }
 
-  getClients(){
+  getClients() {
     return this.clients;
   }
 
-  newClient(client:Client){
+  newClient(client: Client) {
     this.clients.push(client);
   }
 
-  getClient(id:string){
-    this.client = this.af.object('/clients/'+id) as FirebaseObjectObservable<Client>;
+  getClient(id: string) {
+    this.client = this.af.object('/clients/' + id) as FirebaseObjectObservable<Client>;
     return this.client;
+  }
+
+  updateClient(id: string, client: Client) {
+    return this.clients.update(id, client);
   }
 
 }
