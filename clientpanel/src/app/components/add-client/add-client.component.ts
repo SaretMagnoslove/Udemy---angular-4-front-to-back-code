7d8f79930ae5b15c6a827ab79a19f3e4ca4f09ef
SaretMagnoslove <magnoslove@gmail.com>
2018-05-05 15:28:35 +0300

    Clients Table Component

--- clientpanel/src/app/components/add-client/add-client.component.ts
+++ clientpanel/src/app/components/add-client/add-client.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-client.component.css']
 })
 export class AddClientComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
